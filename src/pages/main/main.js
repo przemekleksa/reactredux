eb0cfc4c029aa0c32daea33f0d9745af5d071e0d
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2020-03-01 14:19:09 +0100

    DOM Events

---
+++ src/pages/main/main.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+    state = {
+        name: 'Przemek',
+        age: 34
+    }
+
+    handleClick(e){
+        // this.setState({
+        //     name: "Ela"
+        // })
+        console.log(e.target)
+        
+    }
+
+    handleMouseOver(e){
+        console.log(e.target, e.pageX)
+    }
+
+    handleCopy(e){
+        console.log(e, 'try being original for once!')
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello
+                </h1>
+                <p>Random number: {Math.round(Math.random() * 10)}
+                </p>
+                <h2>My name is {this.state.name}. I'm {this.state.age} years old.</h2> 
+                <button onClick={this.handleClick}>Click Me</button>
+                <button onMouseOver={this.handleMouseOver}>Hover me</button>
+                <p onCopy={this.handleCopy}>What we think, we become</p>
+            </div>
+        )
+    }
+}
+
+export default Main
